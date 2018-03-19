ab0074cd5050f426b4f9bb17f0a1bd355d695114
Paulo V C Medeiros <paulo.medeiros@smhi.se>
2018-03-19 13:42:42 +0100

    Improved install_sys_deps with all main RHEL deps.
    The script should now include all main system dependencies needed for
    compilation on RHEL7, with dependencies of dependencies, as well as
    requirements of rebuilt rpms handled automatically. A new test in a
    clean install is still pending and will be performed soon.

--- install_sys_deps.sh
+++ install_sys_deps.sh
@@ -2,85 +2,97 @@
 
 WORKING_DIR=`pwd`
 RPMBUILD_DIR="${HOME}/rpmbuild"
+SPECS_DIR="${RPMBUILD_DIR}/SPECS"
+COMPILED_RPMS_SYS_DIR="${RPMBUILD_DIR}/RPMS/x86_64"
 TMPDIR='/tmp/obsmon_sys_deps'
 RPM_DEST_DIR="${WORKING_DIR}/obsmon_deps_RHEL7"
-REPO='http://dl.fedoraproject.org/pub/fedora/linux/releases/25/Everything/source/tree/Packages'
+FC25_REPO='http://dl.fedoraproject.org/pub/fedora/linux/releases/25/Everything/source/tree/Packages'
 REPO_LIBKML='http://cbs.centos.org/kojifiles/packages/libkml/1.3.0/3.el7/x86_64'
 
-PKGS="${PKGS} proj-4.9.2-2.fc24.src.rpm"
-PKGS="${PKGS} libgeotiff-1.4.0-7.fc24.src.rpm"
-PKGS="${PKGS} libspatialite-4.3.0a-2.fc24.src.rpm"
-PKGS="${PKGS} ogdi-3.2.0-0.26.beta2.fc24.src.rpm"
-PKGS="${PKGS} gdal-2.1.0-8.fc25.src.rpm"
-LIBKML_PKGS='libkml-1.3.0-3.el7.x86_64.rpm libkml-devel-1.3.0-3.el7.x86_64.rpm'
-PKG_COUNTER=0
+pkgs_append () {
+  PKGS="${PKGS} ${1}"
+}
+yum_pkgs_append () {
+  PKGS_YUM="${PKGS_YUM} ${1}"
+}
 
-
-PKG_COUNTER=$[$PKG_COUNTER +1]
-PKG_RPM_DESTDIR="${RPM_DEST_DIR}/${PKG_COUNTER}_libkml"
-mkdir -p ${PKG_RPM_DESTDIR}
-cd ${PKG_RPM_DESTDIR}
-for PKG in ${LIBKML_PKGS}; do
-  if [ ! -f "${PKG}" ]; then
-    wget ${REPO_LIBKML}/${PKG}
-  fi
-done
+# ${PKGS_YUM}: Pkgs that can be directly installed via yum
+yum_pkgs_append "cairo-devel libXt-devel"
+yum_pkgs_append "libcurl-devel openssl-devel libxml2-devel"
+yum_pkgs_append "mariadb-devel postgresql-devel"
+yum_pkgs_append "geos-devel udunits2-devel v8-314-devel"
+sudo yum -y install ${PKGS_YUM} || { echo "Problems installing ${PKGS_YUM}"; exit 1; }
+# ${PKGS}: Packages that need a bit more work
+# NB.: The order of the pkgs listed below DOES matter.
+pkgs_append "libkml-1.3.0-3.el7.x86_64.rpm"
+pkgs_append "libkml-devel-1.3.0-3.el7.x86_64.rpm"
+pkgs_append "proj-4.9.2-2.fc24.src.rpm"
+pkgs_append "libgeotiff-1.4.0-7.fc24.src.rpm"
+pkgs_append "libspatialite-4.3.0a-2.fc24.src.rpm"
+pkgs_append "ogdi-3.2.0-0.26.beta2.fc24.src.rpm"
+pkgs_append "gdal-2.1.0-8.fc25.src.rpm"
 
 mkdir -p ${TMPDIR}
-for PKG in ${PKGS}; do  
- 
-  PKG_COUNTER=$[$PKG_COUNTER +1]
-  PKG_FIRST_LETTER=${PKG:0:1}
-  PKG_NAME=$(echo "$PKG" | sed 's/-[0-9].*//')
-  PKG_VER=$(echo "$PKG" | sed "s/${PKG_NAME}-//" | sed 's/\.\w*\.src\.rpm$//')
-  PKG_RPM_DESTDIR="${RPM_DEST_DIR}/${PKG_COUNTER}_${PKG_NAME}"
+for PKG in ${PKGS}; do
+  echo "Handling package ${PKG}" 
 
-  if [ -d ${PKG_RPM_DESTDIR} ]; then
-    echo "Dir ${PKG_RPM_DESTDIR} already exists."
-    echo "  > Skipping package ${PKG}."
+  if [[ ! $PKG =~ \.rpm$ ]]; then
+    # If not an *.rpm, assume to be a package name installable via yum
+    sudo yum -y install ${PKG} || { echo "Problems installing ${PKG}"; exit 1; }
     continue
   fi
 
-  cd ${TMPDIR}
-  if [ ! -f "${PKG}" ]; then
-    wget ${REPO}/${PKG_FIRST_LETTER}/${PKG}
-  fi
-
-  rpm --nomd5 -i ${PKG}
-  if [ $? -eq 0 ]; then
-    echo 'Done unpacking RPM'
-    echo ''
+  PKG_NAME=$(echo "$PKG" | sed 's/-[0-9].*//')
+  PKG_VER=$(echo "$PKG" | sed "s/${PKG_NAME}-//" | sed 's/\.\w*\.src\.rpm$//')
+  PKG_FIRST_LETTER=${PKG:0:1}
+  PKG_RPM_DESTDIR="${RPM_DEST_DIR}/${PKG_NAME}"
+  if [[ $PKG_NAME = *"libkml"* ]]; then
+    REPO=${REPO_LIBKML}
   else
-    exit 1
+    REPO=${FC25_REPO}/${PKG_FIRST_LETTER}
   fi
 
-  cd ${RPMBUILD_DIR}/SPECS
-  if [[ $PKG_NAME = *"gdal"* ]]; then
-    sed -i '/%files/a %{_exec_prefix}/etc/bash_completion.d/gdal-bash-completion.sh' ${PKG_NAME}.spec
-    if [ $? -ne 0 ]; then
-      exit 1
+  if [ ! -d ${PKG_RPM_DESTDIR} ]; then
+    # Getting and/or rebuilding needed RPMs
+    cd ${TMPDIR}
+    [ -f "${PKG}" ] || wget ${REPO}/${PKG}
+
+    if [[ $PKG =~ \.src\.rpm$ ]]; then
+      echo "Rebuilding RPM ${PKG}"
+
+      rpm --nomd5 -i ${PKG} || { echo "Problems unpacking ${PKG}"; exit 1; }
+
+      SPEC_FILE=${SPECS_DIR}/${PKG_NAME}.spec
+      if [[ $PKG_NAME = *"gdal"* ]]; then
+        sed -i '/%files/a %{_exec_prefix}/etc/bash_completion.d/gdal-bash-completion.sh' ${SPEC_FILE}
+        if [ $? -ne 0 ]; then
+          echo "Problems editing ${PKG_NAME} spec file."
+          exit 1
+        fi
+      fi
+      REQUIREMENTS=`rpmspec -q --buildrequires --requires ${SPEC_FILE}`
+      [ -n "$REQUIREMENTS" ] && sudo yum -y install ${REQUIREMENTS}
+
+      rm -rf ${COMPILED_RPMS_SYS_DIR}/${PKG_NAME}*-${PKG_VER}*
+      rpmbuild -ba ${SPEC_FILE} || { echo "Problems building ${PKG}"; exit 1; }
+
+      cd ${COMPILED_RPMS_SYS_DIR}
     fi
-  fi
-  REQUIREMENTS=`rpmspec -q --buildrequires --requires ${PKG_NAME}.spec`
-  if [ -n "$REQUIREMENTS" ]; then
-    sudo yum -y install ${REQUIREMENTS}
+
+    mkdir -p ${PKG_RPM_DESTDIR}
+    cp ${PKG_NAME}*-${PKG_VER}* ${PKG_RPM_DESTDIR}
   fi
 
-  rm -rf ${RPMBUILD_DIR}/RPMS/x86_64/${PKG_NAME}*-${PKG_VER}*
-  rpmbuild -ba ${PKG_NAME}.spec
+  cd ${PKG_RPM_DESTDIR}
+  sudo yum localinstall -y ${PKG_NAME}*-${PKG_VER}*
   if [ $? -eq 0 ]; then
-    echo 'Done building RPM'
+    echo "Done with ${PKG}"
     echo ''
   else
+    echo "Problems installing ${PKG_NAME}."
+    echo "You may want to remove the dir ${PKG_RPM_DESTDIR} and try again."
     exit 1
   fi
-
-  cd ${RPMBUILD_DIR}/RPMS/x86_64
-  mkdir -p ${PKG_RPM_DESTDIR}
-
-  cp ${PKG_NAME}*-${PKG_VER}* ${PKG_RPM_DESTDIR}
-  cd ${PKG_RPM_DESTDIR}
-  sudo yum install -y ${PKG_NAME}*-${PKG_VER}*
 
 done
 
