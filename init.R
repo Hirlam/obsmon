69235fa219676b0abde9281c2e8459075e2d017a
Paulo V C Medeiros <paulo.medeiros@smhi.se>
2018-06-27 09:27:02 +0200

    Bugfix in lib search paths for local installs

--- init.R
+++ init.R
@@ -14,7 +14,7 @@ getUserName <- function() {
 userName <- getUserName()
 # Configuring library paths
 localRLibDir <- normalizePath(file.path("utils", "build", "local_R_library"), mustWork=FALSE)
-localInstallRLib <- file.path(localRLibDir, "locally_installed_R_library")
+localInstallRLib <- file.path(localRLibDir, "R-libs")
 userRLib <- Sys.getenv("R_LIBS_USER")
 if(userRLib=="") userRLib <- file.path(Sys.getenv("HOME"), "R", "library")
 userRLib <- normalizePath(userRLib, mustWork=FALSE)
