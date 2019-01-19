2f955935e3d1a43f2253d5c005d0bc930bcb08b6
Paulo V C Medeiros <paulo.medeiros@smhi.se>
2019-01-20 00:26:23 +0100

    Major change to UI to prepare for "oneClick" plots
    Changed UI to navbarPage. The regular obsmon fluidPage will now go in
    one of the tabs (the main tab) of the new UI. The idea is to put the
    option to have "on-click" plots in a separate tab.
    
    The main changes are:
        - A lot of the original code from ui.R has been moved to
            * src/ui/create_main_panel.R
            * src/ui/tabs/main_tab.R
          with some reorganisation of the code (mostly indentation)
        - No longer showing  Obsmon code name as a title in the main page
          (showing in the tabs bar instead)

---
+++ src/ui/create_main_panel.R
@@ -0,0 +1,56 @@
+
+createMainPanel <- function(prependToIds=NULL) {
+  if(is.null(prependToIds)) {
+    tabsetPanelId <- "mainArea"
+    plotOutputId <- "plot"
+    mapTitleOutputId <- "mapTitle"
+    mapOutputId <- "map"
+    queryUsedOutputId <- "queryUsed"
+    dataTableOutputId <- "dataTable"
+  } else {
+    tabsetPanelId <- paste0(prependToIds, "MainArea")
+    plotOutputId <- paste0(prependToIds, "Plot")
+    mapTitleOutputId <- paste0(prependToIds, "MapTitle")
+    mapOutputId <- paste0(prependToIds, "Map")
+    queryUsedOutputId <- paste0(prependToIds, "QueryUsed")
+    dataTableOutputId <- paste0(prependToIds, "DataTable")
+  }
+
+  rtn <- mainPanel(
+    width=9,
+    tabsetPanel(
+      id=tabsetPanelId,
+      tabPanel("Plot", value="plotTab",
+        fluidRow(
+          column(12, align="center",
+            tags$head(tags$style("#plot{height:80vh !important;}")),
+            plotOutput(outputId=plotOutputId) %>% withSpinner(color="#0dc5c1"),
+            tags$style(type="text/css", "body { overflow-y: scroll; }")
+          )
+        )
+      ),
+      tabPanel("Map", value="mapTab",
+        fluidRow(
+          column(12,
+            textOutput(mapTitleOutputId),
+            tags$style(type="text/css", ".shiny-text-output { text-align: center; }")
+          )
+        ),
+        fluidRow(
+          column(12, align="center",
+            tags$head(tags$style("#map{height:80vh !important;}")),
+            leafletOutput(outputId=mapOutputId, height="auto", width="auto")
+              %>% withSpinner(color="#0dc5c1"),
+            tags$style(type="text/css", "body { overflow-y: scroll; }")
+          )
+        )
+      ),
+      tabPanel(value="dataTab", "Query and data",
+        wellPanel(h5("Query used:"), textOutput(queryUsedOutputId)),
+        h5("Data:"),
+        dataTableOutput(dataTableOutputId) %>% withSpinner(color="#0dc5c1")
+      )
+    )
+  )
+  return(rtn)
+}
---
+++ src/ui/tabs/main_tab.R
@@ -0,0 +1,180 @@
+
+cacheButtonsGeneralTooltipMsg <- paste0(
+  "Use the buttons below only if you really need to make changes to the ",
+  "information stored in the cache files.\n",
+  "\n",
+  "NB.: You can produce plots even if cache information is incomplete or ",
+  "unavailable. Obsmon uses caching only to populate the GUI choices with ",
+  "values that are present in the actual data files corresponding to the ",
+  "selected experiment/database/DTG(s) combination."
+)
+recacheCacheButtonTooltipMsg <- paste0(
+  "Refresh cache information available for the selected ",
+  "experiment/database/DTG(s) combination.\n",
+  "\n",
+  "Usage case:\n",
+  "  * The corresponding cached information becomes out-of-date"
+)
+resetCacheButtonTooltipMsg <- paste0(
+  "Erase cache files belonging to the selected experiment and restart ",
+  "caching from scratch.\n",
+  "\n",
+  "The (rare) usage cases include situations when:\n",
+  "  * The cached information becomes completely out-of-date\n",
+  "  * The cache files become inaccessible (e.g., corrupted)\n",
+  "\n",
+  "NB.: This action cannot be undone!"
+)
+
+mainTab <- function() {fluidPage(hidden(div(id="app-content",
+  sidebarLayout(
+    sidebarPanel(
+      width=3,
+      selectizeInput("experiment",
+        multiple=FALSE,
+        label=getDefLabel("experiment"),
+        options=list(placeholder='Loading experiments...'),
+        choices=c()
+      ),
+      selectizeInput("odbBase",
+        label=getDefLabel("odbBase"),
+        options=list(placeholder='Awaiting experiment initialisation...'),
+        multiple=FALSE,
+        choices=c()
+      ),
+      conditionalPanel(
+        condition = "input.odbBase!='ecma_sfc'",
+        selectInput("obtype",
+          label=getDefLabel("obtype"),
+          choices=c()
+        )
+      ),
+      conditionalPanel(
+        condition = "input.obtype!='satem' &&
+                     input.obtype!='scatt' &&
+                     input.obtype!='radar'",
+        selectInput("obname",
+          label=getDefLabel("obname"),
+          choices=c()
+        )
+      ),
+      conditionalPanel(
+        condition = "input.obtype == 'satem'",
+        selectInput("sensor",
+          label=getDefLabel("sensor"),
+          choices=c()
+        ),
+        selectInput("satellite",
+          label=getDefLabel("satellite"),
+          choices=c()
+        ),
+        selectInput("channels",
+          tags$div(getDefLabel("channels"),
+            "(Select",
+            actionLink("channelsSelectAll", "all"),
+            actionLink("channelsSelectNone", "none"),
+            ")"
+          ),
+          choices=c(),
+          multiple=TRUE,
+          selectize=FALSE
+        )
+      ),
+      conditionalPanel(
+        condition = "input.obtype != 'satem'",
+        selectInput("variable",
+          label=getDefLabel("variable"),
+          choices=c()
+        ),
+        conditionalPanel(
+          condition = "input.odbBase != 'ecma_sfc' &&
+                       input.obtype!='scatt' &&
+                       input.obtype!='surface'",
+          selectInput("levels",
+            tags$div(getDefLabel("levels"),
+              "(Select",
+              actionLink("levelsSelectStandard", "standard"),
+              actionLink("levelsSelectAll", "all"),
+              actionLink("levelsSelectNone", "none"),
+              ")"
+            ),
+            choices=c(),
+            multiple=TRUE,
+            selectize=FALSE
+          )
+        )
+      ),
+      selectInput("plottype",
+        label=getDefLabel("plottype"),
+        choices=c()
+      ),
+      conditionalPanel(
+        condition = "input.obtype != 'satem'",
+        selectInput("station",
+          label=getDefLabel("station"),
+          choices=c(),
+          multiple=TRUE,
+          selectize=FALSE
+        )
+      ),
+      fluidRow(
+        column(8, dateInput("date", "Date")),
+        column(4, selectInput("cycle", label="Cycle", choices=c()))
+      ),
+      dateRangeInput("dateRange", "Date Range"),
+      checkboxGroupInput("cycles",
+        label=tags$div("Cycles",
+          "(Select",
+          actionLink("cyclesSelectAll", "all"),
+          actionLink("cyclesSelectNone", "none"),
+          ")"
+        ),
+        inline=TRUE,
+        choices=c()
+      ),
+      conditionalPanel(
+        condition = 'output[["showCacheOptions"]]=="TRUE"',
+        fluidRow(
+          column(12,
+            shiny::icon("exclamation-triangle"),
+            tags$b("Advanced cache-related actions"),
+              HTML('&emsp;'), shiny::icon("info")
+              %>%
+              bs_embed_tooltip(
+                title=cacheButtonsGeneralTooltipMsg,
+                placement="right"
+              ),
+            br(),
+            tags$div(
+              actionButton("recacheCacheButton",
+                "Recache DTG(s)",
+                icon("refresh", lib="glyphicon")
+              ) %>%
+                bs_embed_tooltip(
+                  title=recacheCacheButtonTooltipMsg,
+                  placement="above"
+                ),
+              actionButton("resetCacheButton", "Reset cache files",
+                icon("remove", lib="glyphicon"),
+                style="color: #fff; background-color: #FF0000; border-color: #2e6da4"
+              ) %>%
+                bs_embed_tooltip(
+                  title=resetCacheButtonTooltipMsg,
+                  placement="above"
+                )
+            ),
+            br()
+          )
+        )
+      ),
+      actionButton("doPlot", "Plot", width="100%",
+        icon("ok", lib="glyphicon")
+      ),
+      shinyjs::hidden(actionButton("cancelPlot", "Cancel plot", width="100%",
+        icon("remove", lib="glyphicon"),
+        style="color: #fff; background-color: #FFA500; border-color: #2e6da4"
+      ))
+    ),
+    mainPanel=createMainPanel()
+  )
+)))}
