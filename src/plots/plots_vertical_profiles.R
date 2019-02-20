388d59eb18b5052bbce7f11f9f87d247267cd074
Paulo V C Medeiros <paulo.medeiros@smhi.se>
2019-02-20 17:38:53 +0100

    Add units to varname in vertical profile plot

--- src/plots/plots_vertical_profiles.R
+++ src/plots/plots_vertical_profiles.R
@@ -8,12 +8,14 @@ plotTitle.plotVerticalObsProfile <- function(p, plotRequest, plotData) {
 
 doPlot.plotVerticalObsProfile <- function(p, plotRequest, plotData) {
   df <- data.frame(level=plotData$level, obsvalue=plotData$obsvalue)
+  varname <- plotRequest$criteria$varname
+  ylab <- sprintf("%s [%s]", varname, units[[varname]])
   obplot <- ggplot(data=df) +
     aes(x=level, y=obsvalue) +
     geom_line(colour="black") +
     geom_point(size=2, colour="black") +
     coord_flip() +
-    labs(x="Level", y=plotRequest$criteria$varname)
+    labs(x="Level", y=ylab)
   return(obplot)
 }
 
