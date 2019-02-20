33614ac9e6958c162a2221c4cdacc18cde3ced2d
Paulo V C Medeiros <paulo.medeiros@smhi.se>
2019-02-20 17:40:41 +0100

    New vertical prof plot: Obs, FG & Analysis

--- src/plots/plots_vertical_profiles.R
+++ src/plots/plots_vertical_profiles.R
@@ -19,6 +19,20 @@ doPlot.plotVerticalObsProfile <- function(p, plotRequest, plotData) {
   return(obplot)
 }
 
+doPlot.plotVerticalAnalysisProfile <- function(p, plotRequest, plotData) {
+  localPlotData <- melt(plotData, id=c("level"))
+  varname <- plotRequest$criteria$varname
+  ylab <- sprintf("%s [%s]", varname, units[[varname]])
+  obplot <- ggplot(data=localPlotData) +
+    aes(x=level, y=value, group=variable, colour=variable, shape=variable) +
+    geom_line() +
+    geom_point(size=2) +
+    scale_colour_manual(values=c("black", "blue", "red")) +
+    coord_flip() +
+    labs(x="Level", y=ylab)
+  return(obplot)
+}
+
 registerPlotType("VerticalProfiles",
   plotCreate("plotVerticalObsProfile",
     name="Station Vertical Profile: Obsvalue",
@@ -26,6 +40,22 @@ registerPlotType("VerticalProfiles",
     queryStub=paste(
       "SELECT DISTINCT level, obsvalue FROM usage WHERE %s",
       "ORDER BY level"
+    ),
+    requiredFields=list(
+      "station", "obnumber", "obname", "varname"
+    )
+  )
+)
+
+registerPlotType("VerticalProfiles",
+  plotCreate(c("plotVerticalAnalysisProfile", "plotVerticalObsProfile"),
+    name="Station Vertical Profile: Obs, FG & Analysis",
+    dateType="single",
+    queryStub=paste(
+      "SELECT DISTINCT level, obsvalue,",
+      "(obsvalue-fg_dep) AS first_guess,",
+      "(obsvalue-an_dep) AS analysis",
+      "FROM usage WHERE %s ORDER BY level"
     ),
     requiredFields=list(
       "station", "obnumber", "obname", "varname"
