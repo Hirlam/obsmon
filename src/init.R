0d9ae7d5d7f061585234f408a857c1ae34662e67
Paulo V C Medeiros <paulo.medeiros@smhi.se>
2019-05-21 12:10:31 +0200

    Append major & minor code version info to cacheDir
    To prevent conflict when updating the code. Changes to the way cahing is
    written and read should imply updates in the major or at least minor
    fields of the code version. The patch number has therefore been ignored
    in this particular case, to prevent the creation of too many cache
    directories as obsmon is updated.

--- src/init.R
+++ src/init.R
@@ -115,7 +115,19 @@ configGeneralFillInDefault <- function(config, key, default) {
 
   currentVal <- config$general[[key]]
   config$general[[key]] <- switch(key,
-    "cacheDir"=normalizePath(config$general[[key]], mustWork=FALSE),
+    "cacheDir"={
+      # Append code version information to the cacheDir path. Useful to
+      # prevent conflict when updating the code. Discarding patch, though,
+      # as changes to cache cannot be considered as patches. This avoids the
+      # creation of too many cache directories as obsmon gets updated.
+      obsmonVersionMajorMinor <- paste(
+        unlist(strsplit(obsmonVersion, "\\."))[1:2],
+        collapse="."
+      )
+      normalizePath(
+        file.path(currentVal, paste0("obsmon_v", obsmonVersionMajorMinor)),
+        mustWork=FALSE)
+    },
     "logLevel"={
       if(exists("cmdLineArgs") && isTRUE(cmdLineArgs$debug)) "DEBUG"
       else currentVal
@@ -250,7 +262,7 @@ assertCacheDirWritable <- function(config, verbose=FALSE) {
     msg <- paste(msg, '"[general]" section in your config file.\n')
     stop(msg)
   }
-  if(verbose) flog.info(paste("cacheDir set to:", cacheDirPath, "\n"))
+  if(verbose) flog.info("cacheDir set to: %s\n", cacheDirPath)
 }
 
 configure <- function() {
