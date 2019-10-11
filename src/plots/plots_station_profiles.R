7ceb9b9dd599a2e2be6100003b0688521f7a3f51
Paulo V C Medeiros <paulo.medeiros@smhi.se>
2019-10-11 16:05:09 +0200

    StationProfile plots: Draw curve through mean
    Whenever there's more than one point per level. Continue to show actual
    data points, and join them with a faint line to make it clear that they
    are considered as a group.

--- src/plots/plots_station_profiles.R
+++ src/plots/plots_station_profiles.R
@@ -13,36 +13,67 @@ plotTitle.plotStationProfile <- function(p, plotRequest, plotData) {
 
 doPlot.plotStationProfile <- function(p, plotRequest, plotData) {
   scaleColors <- c(
-    "obsvalue"="black", "fg_dep"="blue", "an_dep"="red",
-    "first_guess"="blue", "analysis"="red",
-    "obsvalue_corrected"="black", "obsvalue_raw"="blue"
+    "obsvalue"="black", "obsvalue_corrected"="black", "obsvalue_raw"="blue",
+    "fg_dep"="blue", "first_guess"="blue",
+    "an_dep"="red", "analysis"="red"
   )
-  localPlotData <- melt(plotData, id=c("level"))
+
+  localPlotData <- plotData %>%
+    group_by(level) %>%
+    melt(id="level", na.rm=TRUE)
+  localPlotDataMean <- plotData %>%
+    group_by(level) %>%
+    summarize_all(mean, na.rm=TRUE) %>%
+    melt(id="level", na.rm=TRUE)
+
+  obplot <- ggplot(localPlotData) +
+    aes(x=level, y=value, group=variable, colour=variable, shape=variable) +
+    scale_colour_manual(values=scaleColors) +
+    geom_point(size=2) +
+    # Draw line through mean values instead of actual data point values
+    geom_line(data=localPlotDataMean, size=1.15)
+  # For clarity: Connect groups of points belonging to the same level
+  if(nrow(localPlotData) != nrow(localPlotDataMean)) {
+    # ggplot doesn't seem to like this when localPlotData==localPlotDataMean
+    # The smaller-sized geom_point added below, for instance, mess up the
+    # legends. Also, for whatever reason, this causes the above added
+    # geom_line to vanish sometimes.
+    obplot <- obplot +
+      # This helps plotly show the mean value. Otherwise, plotly's tooltip
+      # shows "value=NA"
+      geom_point(data=localPlotDataMean, size=0.01) +
+      stat_summary(
+        fun.y=mean, fun.ymin=min, fun.ymax=max, geom="linerange", alpha=0.25
+      )
+  }
+
+  # Axes' labels
   varname <- plotRequest$criteria$varname
   xlab <- levelsLableForPlots(plotRequest$criteria$obnumber, varname)
   ylab <- sprintf("%s [%s]", varname, units[[varname]])
-  obplot <- ggplot(data=localPlotData) +
-    aes(x=level, y=value, group=variable, colour=variable, shape=variable) +
-    geom_point(size=2) +
-    geom_line() +
-    scale_colour_manual(values=scaleColors) +
-    coord_flip_wrapper(default=TRUE) +
-    labs(x=xlab, y=ylab)
-  if(length(names(plotData))<3) {
-    # Legends are not necessary if there's only one x and one y
+  obplot <- obplot + labs(x=xlab, y=ylab)
+
+  # Set x-axis limits and flipping axes
+  # The change in xlim causes ggplotly to omit either the shape or
+  # the line (whichever is added later) from the legend. Not a big
+  # deal but worth pointing out, as this but may be solved in later
+  # releases of the plotly package
+  if(startsWith(tolower(xlab), "pressure")) {
+    xlim <- c(max(refPressures,localPlotData[["level"]]), 0)
+    obplot <- obplot + scale_x_reverse()
+  } else {
+    xlim <- c(0, max(refHeights,localPlotData[["level"]]))
+  }
+  obplot <- obplot + coord_flip_wrapper(default=TRUE, xlim=xlim)
+
+  # Hide legend if there's only one curve to be plotted
+  if(length(unique(localPlotData[["variable"]]))<2) {
     obplot <- obplot + theme(legend.position = "none")
   }
-  if(startsWith(tolower(xlab), "pressure")) {
-    # Using xlim causes ggplotly to omit either the shape or the line
-    # (whichever is added later) from the legend. Not a big deal,
-    # but worth pointing out, as this but may be solved in later
-    # releases of the plotly package
-    obplot <- obplot + xlim(max(refPressures,localPlotData[["level"]]),0)
-  } else {
-    obplot <- obplot + xlim(0, max(refHeights,localPlotData[["level"]]))
-  }
+
   return(obplot)
 }
+
 
 registerPlotType("StationProfiles",
   plotCreate("plotStationProfile",
@@ -68,7 +99,8 @@ registerPlotType("StationProfiles",
       "(obsvalue-fg_dep) AS first_guess,",
       "(obsvalue-an_dep) AS analysis",
       "FROM usage WHERE %s AND",
-      "(fg_dep IS NOT NULL) AND (an_dep IS NOT NULL) AND (obsvalue IS NOT NULL)",
+      "(fg_dep IS NOT NULL) AND",
+      "(an_dep IS NOT NULL) AND (obsvalue IS NOT NULL)",
       "ORDER BY level"
     ),
     requiredFields=list(
