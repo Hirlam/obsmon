98dcc30b61faa03eba1f4167489692a1e879649a
Paulo V C Medeiros <paulo.medeiros@smhi.se>
2019-11-05 14:43:18 +0100

    Initial work on station diag plots interactivity
    Converting ggplots using plotly and combining them using subplots

--- src/plots/plots_diagnostic.R
+++ src/plots/plots_diagnostic.R
@@ -15,7 +15,19 @@ statisticsPanel <- function(data, column, bw, fill, interactive=FALSE) {
     qq <- ggplot(data) +
       aes(sample=column) +
       stat_qq()
-    grid.arrange(hist, ecdf, qq, ncol=3)
+    if(interactive) {
+      plotly::subplot(
+        ggplotly(hist) %>% layout(yaxis=list(title="count")),
+        ggplotly(ecdf) %>% layout(yaxis=list(title="ECDF")) ,
+        ggplotly(qq) %>% layout(yaxis=list(title="sample")),
+        nrows=1,
+        margin=c(0.06, 0, 0, 0), # left, right, top and bottom
+        titleY=TRUE,
+        titleX=TRUE
+      )
+    } else {
+      grid.arrange(hist, ecdf, qq, ncol=3)
+    }
   }, list(column=columnName)))
 }
 
@@ -29,15 +41,6 @@ plotTitle.plotDiagnostic <- function(p, plotRequest, plotData) {
     plotRequest$dbType, formatDtg(crit$dtg), crit$obname, crit$varname
   )
   return(title)
-}
-
-# Disable interactivity for "Station Diagnostics" plots.
-# Cannot be directly converted from ggplot to plotly due to the use of
-# grid.arrange
-plotGenerate.plotDiagnostic <- function(
-  p, plotRequest, plotData, interactive
-) {
-  plotGenerate.default(p, plotRequest, plotData, interactive=FALSE)
 }
 
 doPlot.plotDiagnostic <- function(
@@ -83,25 +86,90 @@ doPlot.plotDiagnostic <- function(
     geom_point() +
     facet_grid(panel~., scales="free_y") +
     scale_color_manual(labels=info$labels, values=info$colors) +
-    labs(y=sprintf("%s [%s]", varname, units[[varname]]))
+    labs(y=sprintf("%s [%s]", varname, units[[varname]])) +
+    theme(legend.title=element_blank())
   maxval <- max(plotData$fg_dep, plotData$an_dep)
   minval <- min(plotData$fg_dep, plotData$an_dep)
   bw <- (maxval-minval)/20.
-  panels <- list(comparison,
-                 statisticsPanel(plotData, fg_dep, bw, info$colors[["fg"]]))
-  if (hasMinimization) {
-    lay <- rbind(c(1),
-                 c(1),
-                 c(2),
-                 c(3))
-    panels <- c(panels,
-                list(statisticsPanel(plotData, an_dep, bw, info$colors[["an"]])))
+
+  if(interactive) {
+    interactiveComparisonPlot <- ggplotly(comparison) %>%
+      layout(
+        legend=list(
+          orientation="v",
+          xanchor="left", x=1.025,
+          yanchor="center", y=0.75
+        ),
+        # Axis labels vanish with subplot. Adding them back (y now, x below).
+        yaxis=list(title=sprintf("%s [%s]", varname, units[[varname]]))
+      ) %>%
+    # Add x-label as an annotation. It sits too far from the axis otherwise.
+    add_annotations(
+      # x-asis label
+      text="DATE",
+      showarrow=FALSE,
+      xref="paper", xanchor="center", x=0.5,
+      yref="paper", yanchor="top", y=-0.1
+    )
+    # Constructing the final plot
+    fgDepStatPanel <- statisticsPanel(
+      plotData, fg_dep, bw, info$colors[["fg"]], interactive=TRUE
+    )
+    if (hasMinimization) {
+      andDepStatPanel <- statisticsPanel(
+        plotData, an_dep, bw, info$colors[["an"]], interactive=TRUE
+      )
+      obplot <- plotly::subplot(
+        interactiveComparisonPlot,
+        fgDepStatPanel,
+        andDepStatPanel,
+        heights=c(0.4, 0.3, 0.3),
+        titleX=TRUE,
+        titleY=TRUE,
+        margin=c(0, 0, 0.1125, 0), # left, right, top and bottom
+        nrows=3
+      )
+    } else {
+      obplot <- plotly::subplot(
+        interactiveComparisonPlot,
+        fgDepStatPanel,
+        heights=c(0.45, 0.45),
+        titleX=TRUE,
+        titleY=TRUE,
+        margin=c(0, 0, 0.1125, 0), # left, right, top and bottom
+        nrows=2
+      )
+    }
+    obplot <- obplot %>%
+      layout(
+        showlegend=TRUE,
+        margin = list(t=130, l=80, b=50)
+      ) %>%
+      config(
+        edits=list(axisTitleText=FALSE)
+      )
+
   } else {
-    lay <- rbind(c(1),
-                 c(2))
+    panels <- list(comparison,
+                   statisticsPanel(plotData, fg_dep, bw, info$colors[["fg"]]))
+    if (hasMinimization) {
+      lay <- rbind(c(1),
+                   c(1),
+                   c(2),
+                   c(3))
+      panels <- c(panels,
+                  list(statisticsPanel(plotData, an_dep, bw, info$colors[["an"]])))
+    } else {
+      lay <- rbind(c(1),
+                   c(2))
+    }
+    obplot <- grid.arrange(grobs=panels, layout_matrix=lay)
   }
-  obplot <- grid.arrange(grobs=panels, layout_matrix=lay)
   obplot
+}
+
+doPlotly.plotDiagnostic <- function(...) {
+  doPlot.plotDiagnostic(..., interactive=TRUE)
 }
 
 registerPlotType(
