65d1a2337705cbf81f4c2fea4fe924e68d10faba
Paulo V C Medeiros <paulo.medeiros@smhi.se>
2021-03-23 18:31:55 +0100

    Simplify function getDefLabel

--- global.R
+++ global.R
@@ -32,23 +32,17 @@ idleTimer();",
 
 
 defaultMenuLabels <- list(
-  experiment="Experiment",
   odbBase="Data Assimilation Category/Database",
   obtype="Observation Type",
   obname="Observation Name",
-  sensor="Sensor",
-  satellite="Satellite",
   scatt_satellite="Satellite",
-  channels="Channels",
-  variable="Variable",
-  levels="Levels"
   plottype="Type of Plot",
-  station="Station"
+  stationSingle="Station"
 )
 
 getDefLabel <- function(inputId) {
   rtn <- defaultMenuLabels[[inputId]]
-  if(is.null(rtn)) rtn <- inputId
+  if(is.null(rtn)) rtn <- stringr::str_to_title(inputId)
   return(rtn)
 }
 
